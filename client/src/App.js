e4e6a9cf6b715316b6d33f1ef0bb3e5597ceae47
And4Web <anands2807@gmail.com>
2022-09-13 15:46:53 +0530

    initial setup done and multiple pages with routes created.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Healthy</h1>
+    <div >
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/login" element={<Login/>}/>
+        <Route exact path="/register" element={<Register/>}/>
+      </Routes>   
+      <Footer/>
     </div>
   );
 }
