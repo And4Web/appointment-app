bd27f9d723f032490f853f20da04f0d31b045315
And4Web <anands2807@gmail.com>
2022-09-13 17:59:01 +0530

    register and login pages created.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import {Form, Input, Button} from 'antd';
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='authentication'>
+      <div className='authentication-form card p-3'>
+        <h1 className=' card-title '>Welcome, Login here</h1>
+        <Form layout='vertical'>         
+          <Form.Item label="Email" name="email">
+            <Input placeholder='Enter your email'/>
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input placeholder='Enter your password'/>
+          </Form.Item>
+          <Button type='primary'>LOGIN</Button>
+        </Form>
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {Form, Input, Button} from 'antd';
 
 function Register() {
   return (
-    <div>Register</div>
+    <div className='authentication'>
+      <div className='authentication-form card p-3'>
+        <h1 className=' card-title '>Welcome, Register here</h1>
+        <Form layout='vertical'>
+          <Form.Item label="Name" name="name">
+            <Input placeholder='Enter your name'/>
+          </Form.Item>
+          <Form.Item label="Email" name="email">
+            <Input placeholder='Enter your email'/>
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input placeholder='Enter your password'/>
+          </Form.Item>
+          <Button type='primary'>REGISTER</Button>
+          <Link to='/login' className='anchor'>Click here to login</Link>
+        </Form>
+      </div>
+    </div>
   )
 }
 
