efc11c7731a8ab687cd6db3b275fbf24b604260b
unknown <anands2807@gmail.com>
2023-10-31 14:43:03 +0530

    apply-doctor feature with notification to admin with the notification badge in the frontend done.

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,117 +1,148 @@
-import React,{useEffect, useState} from 'react'
-import '../layout.css'
-import { Link, useLocation, useNavigate } from 'react-router-dom'
-import axios from 'axios';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import "../layout.css";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { Badge } from "antd";
 
-function Layout({children}) {
-  const userState = useSelector(state=>state.user.user);
+function Layout({ children }) {
+  const userState = useSelector((state) => state.user.user);
   const location = useLocation();
   const navigate = useNavigate();
 
   const [collapsed, setCollapsed] = useState(false);
   const [user, setUser] = useState("");
-  
+  const [unseenNotifications, setUnseeNotifications] = useState([]);
+
   const [isAdmin, setIsAdmin] = useState(false);
-  
+
   let userFirstName = user.split(" ")[0];
 
-  useEffect(()=>{
-    if(userState){
+  useEffect(() => {
+    if (userState) {
       setUser(userState.name);
-      setIsAdmin(userState.isAdmin)
+      setIsAdmin(userState.isAdmin);
+      setUnseeNotifications(userState.unseenNotifications);
     }
-
-  },[userState])  
-
+  }, [userState]);
 
   // console.log(userState, isAdmin)
+  // console.log("layout.js, unseenNotifications: ", unseenNotifications)
+  // console.log("layout.js user state: ", userState);
 
   const userMenu = [
     {
       name: "Home",
       path: "/",
-      icon: "ri-home-line"
+      icon: "ri-home-line",
     },
     {
       name: "Appointments",
       path: "/appointments",
-      icon: "ri-list-check"
+      icon: "ri-list-check",
     },
     {
       name: "Apply doctor",
       path: "/apply-doctor",
-      icon: "ri-hospital-line"
+      icon: "ri-hospital-line",
     },
     {
       name: "Profile",
       path: "/profile",
-      icon: "ri-profile-line"
+      icon: "ri-profile-line",
     },
-   
-  ]
+  ];
   const adminMenu = [
     {
       name: "Home",
       path: "/",
-      icon: "ri-home-line"
+      icon: "ri-home-line",
     },
     {
       name: "Users",
       path: "/users",
-      icon: "ri-team-line"
+      icon: "ri-team-line",
     },
     {
       name: "Doctors",
       path: "/doctors",
-      icon: "ri-user-heart-fill"
+      icon: "ri-user-heart-fill",
     },
     {
       name: "Profile",
       path: "/profile",
-      icon: "ri-profile-line"
+      icon: "ri-profile-line",
     },
-    
-  ]
+  ];
 
   const menuToBe = isAdmin ? adminMenu : userMenu;
-  
-  return (    
-      <div className="main">
-        <div className="d-flex layout">
-          <div className={collapsed ? "collapsed-sidebar":"sidebar"}>
-            <div className="sidebar-header">
-              <h1 className='site-logo'>AH</h1>
-            </div>
-            <div className="sidebar-menu">
-              {menuToBe.map((menu, index)=>{
-                const isActive = location.pathname === menu.path;
-                return(
-                  <div className={`d-flex ${!collapsed? "menu-item" : "collapsed-menu-item"} ${isActive && "active-menu-item"}`} key={index} onClick={()=>navigate(`${menu.path}`)}>
-                    <i className={menu.icon} ></i>
-                    {!collapsed ? (<Link to={menu.path}>{menu.name}</Link>) : null}                    
-                  </div>
-                )
-              })}
-              <div className={`d-flex ${!collapsed? "menu-item" : "collapsed-menu-item"} "active-menu-item"`} onClick={()=>{localStorage.clear(); navigate("/login")}}>
-                    <i className="ri-logout-circle-line" ></i>
-                    {!collapsed ? (<Link to="/logout">Logout</Link>) : null}                    
-                  </div>
-            </div>
+
+  return (
+    <div className="main">
+      <div className="d-flex layout">
+        <div className={collapsed ? "collapsed-sidebar" : "sidebar"}>
+          <div className="sidebar-header">
+            <h1 className="site-logo">AH</h1>
           </div>
-          <div className="content">
-            <div className="header">
-              {collapsed? (<i className="ri-menu-2-line header-action-icon" onClick={()=>setCollapsed(false)}></i>) : (<i className="ri-close-line header-action-icon" onClick={()=>setCollapsed(true)}></i>)}   
-              <div className="d-flex align-items-center px-3 header-right"><i className="ri-notification-line header-action-icon px-3" ></i>
-              <Link to="/profile" className='header-user'>{userFirstName}</Link>
-              </div>        
+          <div className="sidebar-menu">
+            {menuToBe.map((menu, index) => {
+              const isActive = location.pathname === menu.path;
+              return (
+                <div
+                  className={`d-flex ${
+                    !collapsed ? "menu-item" : "collapsed-menu-item"
+                  } ${isActive && "active-menu-item"}`}
+                  key={index}
+                  onClick={() => navigate(`${menu.path}`)}
+                >
+                  <i className={menu.icon}></i>
+                  {!collapsed ? <Link to={menu.path}>{menu.name}</Link> : null}
+                </div>
+              );
+            })}
+            <div
+              className={`d-flex ${
+                !collapsed ? "menu-item" : "collapsed-menu-item"
+              } "active-menu-item"`}
+              onClick={() => {
+                localStorage.clear();
+                navigate("/login");
+              }}
+            >
+              <i className="ri-logout-circle-line"></i>
+              {!collapsed ? <Link to="/logout">Logout</Link> : null}
             </div>
-            <div className="content-body">{children}</div>
           </div>
         </div>
+        <div className="content">
+          <div className="header">
+            {collapsed ? (
+              <i
+                className="ri-menu-2-line header-action-icon"
+                onClick={() => setCollapsed(false)}
+              ></i>
+            ) : (
+              <i
+                className="ri-close-line header-action-icon"
+                onClick={() => setCollapsed(true)}
+              ></i>
+            )}
+            <div className="d-flex align-items-center px-3 header-right">
+              <div className="notification">
+                <Badge size="default" count={unseenNotifications.length} className="notification-badge">
+                  <i className="ri-notification-line header-action-icon px-3 notification-bell"></i>
+                </Badge>
+              </div>
+              <Link to="/profile" className="header-user mx-2">
+                {userFirstName}
+              </Link>
+            </div>
+          </div>
+          <div className="content-body">{children}</div>
+        </div>
       </div>
-    
-  )
+    </div>
+  );
 }
 
-export default Layout
+export default Layout;
